72813176dc2f45cd2a288218b20bf1e1ea8199bf
Nithin S <nithin19484@gmail.com>
2015-01-27 18:46:52 +0530

    Adding liscence terms

--- core/main.cpp
+++ core/main.cpp
@@ -1,3 +1,24 @@
+/*=========================================================================
+
+  Program:   mscomplex3d
+
+  Copyright (c) Nithin Shivashankar, Vijay Natarajan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=========================================================================*/
+
 #include <boost/program_options.hpp>
 
 #include <grid_dataset.h>
