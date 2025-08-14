163f53f106b5bb1cc9dee95be8350e9d9b9f7f7e
Santripta <santripta@gmail.com>
2025-08-14 12:45:46 +0530

    build: reintroduce setup.py

--- pyms3d_core/__init__.py
+++ pyms3d_core/__init__.py
@@ -1,6 +1,1 @@
-# ugly hack to make package structure consistent across operating systems
-
-try:
-    from .pyms3d_core.pyms3d_core import *
-except ImportError:
-    from .pyms3d_core import *  # this loads all symbols from your compiled module
+from .pyms3d_core import *  # this loads all symbols from your compiled module
